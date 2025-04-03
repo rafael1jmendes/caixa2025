8db89444314fe285a50893a9eb0271d8398e64bb
Rafael Mendes <rafael1jmendes@gmail.com>
2025-04-02 23:12:51 -0300

    adc consultas caixa

---
+++ src/main/java/com/mendes/caixa2025/controller/ConsultaCaixaController.java
@@ -0,0 +1,10 @@
+package com.mendes.caixa2025.controller;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(schema = "caixa2025", name = "view_fluxo_caixa")
+public class ConsultaCaixaController {
+
+}
