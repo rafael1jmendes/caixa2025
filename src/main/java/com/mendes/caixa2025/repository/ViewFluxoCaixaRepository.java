426e6e27effacf9f7cd88407fb61feb7c687f09e
Wisley Lobo <92831271+wisleylobo@users.noreply.github.com>
2025-04-15 13:21:56 -0300

    Update ViewFluxoCaixaRepository.java

--- src/main/java/com/mendes/caixa2025/repository/ViewFluxoCaixaRepository.java
+++ src/main/java/com/mendes/caixa2025/repository/ViewFluxoCaixaRepository.java
@@ -1,12 +1,11 @@
 package com.mendes.caixa2025.repository;
 
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.cdi.JpaRepositoryExtension;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.mendes.caixa2025.model.ViewFluxoCaixa;
 
 
 @Repository
-public interface ViewFluxoCaixaRepository extends JpaRepositoryExtension<ViewFluxoCaixa, Long>  {
+public interface ViewFluxoCaixaRepository extends JpaRepository<ViewFluxoCaixa, Long>  {
 
 }
